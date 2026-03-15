cba7b1fcef3ffc520db9d6655b9188ab890165c8
Anshuman Sharma <162892791+anshumansharmaa@users.noreply.github.com>
2026-03-15 21:07:21 +0530

    Delete MultipleInheritance/src/com/personal/multipleInheritance/ExecutionA1.java

--- MultipleInheritance/src/com/personal/multipleInheritance/ExecutionA1.java
+++
@@ -1,9 +0,0 @@
-package com.personal.multipleInheritance;
-
-public class ExecutionA1 {
-	public static void main(String[] args) {
-		Information I = new Information();
-		I.speak();
-		I.sound();
-	}
-}
